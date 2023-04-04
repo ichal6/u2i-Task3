b3032fef170658bd1513494a12e01c364a2e8b20
Michael Lechowicz <ichal.lechowicz@gmail.com>
2023-04-04 17:06:14 +0200

    Refactor code

--- src/Solution.java
+++ src/Solution.java
@@ -1,17 +1,32 @@
 import java.time.DayOfWeek;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
 
 class Solution {
     public int solution(String S) {
-        String[] meetings = S.strip().split("\n");
-        Arrays.stream(meetings).forEach(System.out::println);
+        boolean[] busyMinutesSlot = getBusyArray(S);
+        // Find the longest time slot when James can sleep
+        int sleepTimeInMinutes = 0;
+        int possibleSleep = 0;
+        for (boolean busySlot : busyMinutesSlot) {
+            if (!busySlot) {
+                possibleSleep++;
+            } else if (possibleSleep != 0) {
+                sleepTimeInMinutes = Math.max(sleepTimeInMinutes, possibleSleep);
+                possibleSleep = 0;
+            }
+        }
+        sleepTimeInMinutes = Math.max(sleepTimeInMinutes, possibleSleep);
 
+        return sleepTimeInMinutes;
+    }
+
+    private boolean[] getBusyArray(String schedule) {
+        String[] meetings = schedule.strip().split("\n");
         // Create an array of boolean flags for each minute of the week
-        boolean[] busy = new boolean[7 * 24 * 60];
+        boolean[] busyMinutesSlot = new boolean[7 * 24 * 60];
 
-        // Parse each meeting and mark its time slot as busy in the array
+        // Parse each meeting
         for (String meeting : meetings) {
             String[] parts = meeting.split(" ");
             DayOfWeek dayOfWeek = fromAbbreviation(parts[0]);
@@ -19,37 +34,21 @@ class Solution {
             LocalTime startTime = LocalTime.parse(times[0], DateTimeFormatter.ofPattern("HH:mm"));
             LocalTime endTime = LocalTime.parse(times[1], DateTimeFormatter.ofPattern("HH:mm"));
             int startMinute = (dayOfWeek.getValue() -1) * 24 * 60 + startTime.getHour() * 60 + startTime.getMinute();
-            int endMinute = 0;
+            int endMinute;
             if(endTime.equals(LocalTime.MIDNIGHT)){
                 endMinute = (dayOfWeek.getValue() -1) * 24 * 60 + 24 * 60;
             } else{
                 endMinute = (dayOfWeek.getValue() -1) * 24 * 60 + endTime.getHour() * 60 + endTime.getMinute();
             }
-
+            // Mark time slot as busy in the array
             for (int i = startMinute; i < endMinute; i++) {
-                busy[i] = true;
+                busyMinutesSlot[i] = true;
             }
         }
-        // Find the longest time slot when James can sleep
-        int longestSleep = 0;
-        int currentSleep = 0;
-        boolean busyMinute = busy[0];
-        for (int i = 0; i < busy.length; i++) {
-            if(i > 0)
-                busyMinute = busy[i-1];
-            if (!busy[i]) {
-                currentSleep++;
-            } else if(currentSleep != 0) {
-                longestSleep = Math.max(longestSleep, currentSleep);
-                currentSleep = 0;
-            }
-        }
-        longestSleep = Math.max(longestSleep, currentSleep);
-
-        // Return the result in minutes
-        return longestSleep;
+        return busyMinutesSlot;
     }
 
+    // Parse day of month abbreviation to DayOfWork instance
     private DayOfWeek fromAbbreviation(String abbr){
         switch (abbr) {
             case "Mon":
